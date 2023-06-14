aff4d718a1778104c8f9e8fb00f8e52a77759985
Adrien Le Doussal <adoussal@gmail.com>
2023-06-14 16:32:46 +0200

    rails Day 3 Exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants = [
+  {
+    name: 'Epicure',
+    address: '75008 Paris',
+    category: 'french',
+  },
+  {
+    name: 'La Palette',
+    address: '75005 Paris',
+    category: 'french',
+  },
+  {
+    name: 'The Fat Duck',
+    address: '75009 Paris',
+    category: 'french',
+  },
+  {
+    name: 'Le Petit Cambodge',
+    address: '75010 Paris',
+    category: 'thai',
+  },
+  {
+    name: 'Il Carpaccio',
+    address: '75011 Paris',
+    category: 'italian',
+  }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
